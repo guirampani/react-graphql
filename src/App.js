63b88f7a733ec324f04321fa65f52c3efe9df8e6
Gui <guirampsil@gmail.com>
2020-11-15 20:22:27 +1100

    Getting it into github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import React, { Component } from 'react';
+// import { Line, Bar } from 'react-chartjs-2';
+// import graphql2chartjs from 'graphql2chartjs';
+// import { Query } from 'react-apollo';
+// import gql from 'graphql-tag';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+// class App extends Component {
+
+//   render() {
+//     const data = {
+//       "labels": ["label1", "label2", "label3", "label4"],
+//       "datasets": [{
+//         "label": "Sample dataset",
+//         "data": [45, 23, 56, 55],
+//         "pointBackgroundColor": ["red", "brown", "green", "yellow"],
+//         "borderColor": "brown", 
+//         "fill": false
+//       }]
+//     }
+    
+//     return (
+//       <div
+//         style={{display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '20px'}}
+//       >
+//         <Line
+//           data={data}
+//         />
+//       </div>
+//     );
+//   }
+// };
+
+// const App = () => (
+//   <Query query = {gql`
+//   query {
+//     releases_by_status {
+//       label: release_status
+//       data: count
+//     }
+//   }
+//   `}>{({loading, error, data}) => {
+
+//     if (data) {
+//       const g2c = new graphql2chartjs(data, 'bar');
+//       return <Bar data = {g2c.data} />
+//     } else {
+//       return 'Loading/error';
+//     }
+//   }}
+
+
+//   </Query>
+// );
+
+
+import React from 'react';
+import './App.css';
+//import NavBar from './Navbar';
+import { BasicBarChart } from './charts/BasicBarChart';
+
+
+const App = () => (
+  <div>
+    <div style={{margin: '10px', paddingTop: '65px'}}>
+      <BasicBarChart/><p>hllooo</p>
+     </div>
+  </div>
+);
+
+// class App extends Component {
+
+//   render() {
+//     return (
+//      <div>
+//       <div style={{margin: '10px', paddingTop: '65px'}}>
+//       <BasicBarChart/><p>hellooooo</p>
+//       </div>
+//     </div>
+//     );
+//   }
+// };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const link = new WebSocketLink({
+  uri: 'ws://localhost:8080/v1/graphql',
+  options: {
+    reconnect: true
+  }
+})
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  ),
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
