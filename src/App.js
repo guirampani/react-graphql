f121fc1aaa28f14018e5f5569758aacb956fb45c
Gui Rampani <guilherme.rampani@plutora.com>
2020-11-23 11:21:12 +1100

    Latest

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BasicBarChart } from './charts/BasicBarChart';
+import { LineChart } from './charts/LineChart';
 
 
 const App = () => (
   <div>
-    <div style={{margin: '10px', paddingTop: '65px'}}>
-      <BasicBarChart/><p>hllooo</p>
-     </div>
+    <div style={{margin: '10px', width: '700px', height: '900px'}}>
+      <BasicBarChart/>
+      <LineChart/>
+    </div>
   </div>
 );
 
--- src/charts/LineChart.js
+++ src/charts/LineChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Doughnut } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 //import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 //import SyntaxHighlighter from 'react-syntax-highlighter';
 import graphql2chartjs from 'graphql2chartjs';
@@ -8,11 +8,11 @@ import gql from 'graphql-tag';
 
 const query = `
     query {
-        releases_by_status {
-            label: release_status
-            data: count
+        releases_by_month {
+          label: month
+          data: count
         }
-    }`;
+      }`;
 
 // Chart component
 const Chart = ({ query }) => (
@@ -29,18 +29,20 @@ const Chart = ({ query }) => (
           console.log(dataPoint.label);
           console.log(datasetName);
           return {
-            chartType: 'doughnut',
-            backgroundColor: dataPoint.label === 'Draft' ? '#64B2F7' : (dataPoint.label === 'In Progress' ? "#F78C42" : "#6EF164"),
+            chartType: 'line',
+            borderColor: "#6EF164",
+            fill: false
           }
         });
 
         return (
-          <Doughnut data={g2c.data} 
+          <Line data={g2c.data} 
           options={
                     { legend: {
                         display: true,
                         reverse: true
-                      }
+                      },
+                      //fill: false
                     }
                   } />
         )
@@ -51,12 +53,12 @@ const Chart = ({ query }) => (
 
 /****************************************UTILS*****************************************/
 
-const BasicBarChart = ({ path }) => {
+const LineChart = ({ path }) => {
   return (
     <div style={{margin: '10px'}}>
       <div key="bar">
-        <div style={{marginBottom: '20px'}} id="doughnut">
-            <h2 style={{margin: '10px', textAlign: 'center'}}>Doughnut Chart</h2>
+        <div style={{marginBottom: '20px'}} id="line">
+            <h2 style={{margin: '10px', textAlign: 'center'}}>Line Chart</h2>
             <div className="chartWrapper">
               <div className="half_screen">
                 <Chart query={query}/>
@@ -69,4 +71,4 @@ const BasicBarChart = ({ path }) => {
   );
 }
 
-export { BasicBarChart };
+export { LineChart };
